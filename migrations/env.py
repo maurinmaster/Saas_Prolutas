318cc69fb61b080e5053fb929a5ca5dab9303113
maurinmaster <procombatesanda@gmail.com>
2025-09-19 19:14:30 -0300

    Atualização

--- migrations/env.py
+++ migrations/env.py
@@ -4,6 +4,10 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from servico_contas.models import Base as BaseContas
+from servico_usuarios.models import Base as BaseUsuarios
+from servico_contas.models import Academy
+from servico_usuarios.models import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = BaseUsuarios.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
